e53bb329d4c7b062061cc6a83c6b7c8938c6874b
Eric-Cortez <ecrcortez@gmail.com>
2025-10-27 23:49:34 -0700

    fix: update comment for clarity on Firebase Server App initialization

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -34,8 +34,9 @@ export async function getAuthenticatedAppForUser() {
   // Initialize Firebase Server App with client app configuration and auth token
   const firebaseServerApp = initializeServerApp(
     // https://github.com/firebase/firebase-js-sdk/issues/8863#issuecomment-2751401913
+    // TOGGLE BETWEEN USING DEFAULT CONFIG AND ENV CONFIG for local testing
     // initializeApp(firebaseConfig), // Initialize base Firebase app with default configuration
-    initializeApp(), 
+    initializeApp(),
     {
       authIdToken, // Pass the ID token for authentication
     }
