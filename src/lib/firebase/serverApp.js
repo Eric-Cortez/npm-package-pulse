b3d4a5986520e43e8bbbb2a92b0c79eef907b7d9
Eric-Cortez <ecrcortez@gmail.com>
2025-10-27 23:31:29 -0700

    fix: remove local .env config

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -34,7 +34,8 @@ export async function getAuthenticatedAppForUser() {
   // Initialize Firebase Server App with client app configuration and auth token
   const firebaseServerApp = initializeServerApp(
     // https://github.com/firebase/firebase-js-sdk/issues/8863#issuecomment-2751401913
-    initializeApp(firebaseConfig), // Initialize base Firebase app with default configuration
+    // initializeApp(firebaseConfig), // Initialize base Firebase app with default configuration
+    initializeApp(), 
     {
       authIdToken, // Pass the ID token for authentication
     }
